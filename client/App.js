e244f14932f6d33863692217f96eea9532bad6c9
Nathan Quach <nathanquach8x@gmail.com>
2018-06-20 00:26:19 +1000

    Add Open Sans font

--- client/App.js
+++ client/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
+import { injectGlobal } from 'emotion';
 import MangaBoard from './components/templates/MangaBoard';
+
+injectGlobal`
+  * {
+    box-sizing: border-box;
+  }
+
+  html, body {
+    font-family: 'Open Sans', sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+`
 
 export default class App extends Component {
   constructor(props) {

c2d3904dd49de9b3ee212bb3effdb77b066bcb93
Nathan Quach <nathanquach8x@gmail.com>
2018-06-19 16:41:31 +1000

    Add created, updated datetime to models

--- common/models/manga.js
+++ common/models/manga.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = function(Manga) {
-
+  Manga.observe('before save', async function(ctx, next) {
+    if (ctx.instance) {
+      ctx.instance.updated = new Date();
+    } else {
+      ctx.data.created = ctx.data.created || new Date();
+      ctx.data.updated = new Date();
+    }
+    next();
+    return;
+  });
 };
--- common/models/page.js
+++ common/models/page.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = function(Page) {
-
+  Page.observe('before save', async function(ctx, next) {
+    if (ctx.instance) {
+      ctx.instance.updated = new Date();
+    } else {
+      ctx.data.created = ctx.data.created || new Date();
+      ctx.data.updated = new Date();
+    }
+    next();
+    return;
+  });
 };

b19a829c8d1a1b327b9df36d4ff2e944822b991b
Nathan Quach <nathanquach8x@gmail.com>
2018-06-28 01:06:02 +1000

    Move data query logic into PageList

--- client/components/organisms/PagesPanel.js
+++ client/components/organisms/PagesPanel.js
@@ -1,10 +1,8 @@
 import React, { PureComponent } from 'react';
 import styled from 'react-emotion';
-import { fetchItems, countItems } from '../Datasource';
 import PageList from './PageList';
-import Paginator from '../molecules/Paginator';
 
-const PagesPaginator = styled(Paginator)`
+const StyledPageList = styled(PageList)`
   ul {
     flex-direction: row;
     flex-wrap: wrap;
@@ -19,52 +17,9 @@ const PagesPaginator = styled(Paginator)`
   }
 `
 
-const api = '/api/Pages';
-
 export default class PagesPanel extends PureComponent {
   constructor(props) {
     super(props);
-    this.countItems = this.countItems.bind(this);
-    this.fetchItems = this.fetchItems.bind(this);
-    this.renderList = this.renderList.bind(this);
-  }
-
-  countItems() {
-    const manga = this.props.manga;
-    if (manga === null) {
-      return new Promise((resolve, reject) => {
-        resolve();
-      });
-    }
-
-    const where = { mangaId: manga.id };
-    return countItems(api, where);
-  }
-
-  fetchItems(skip, itemPerPage) {
-    const manga = this.props.manga;
-    if (manga === null) {
-      return new Promise((resolve, reject) => {
-        resolve();
-      });
-    }
-
-    const filter = {
-      where: { mangaId: manga.id },
-      order: 'title'
-    };
-    return fetchItems(api, filter, skip, itemPerPage);
-  }
-
-  renderList(items, loadPrevList, loadNextList) {
-    return (
-      <PageList
-        items={items}
-        loadPrevList={loadPrevList}
-        loadNextList={loadNextList}
-        onItemClick={this.props.onItemClick}
-      />
-    );
   }
 
   render() {
@@ -74,12 +29,10 @@ export default class PagesPanel extends PureComponent {
       <section>
         {manga ? (<h3>{manga.title}</h3>) : ''}
 
-        <PagesPaginator
+        <StyledPageList
           manga={this.props.manga}
-          fetchItems={this.fetchItems}
-          countItems={this.countItems}
           itemsPerPage={this.props.itemsPerPage}
-          render={this.renderList}
+          onItemClick={this.props.onItemClick}
         />
       </section>
     );

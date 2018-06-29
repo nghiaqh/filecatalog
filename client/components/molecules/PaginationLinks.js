0c3dadc3c12076ca7188a03618452acb8a792d8c
Nathan Quach <nathanquach8x@gmail.com>
2018-06-30 05:43:48 +1000

    Restyle paginator

--- client/components/molecules/PaginationLinks.js
+++ client/components/molecules/PaginationLinks.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import styled from 'react-emotion';
-
-const Box = styled('div')`
-  margin: 20px 0;
-  font-weight: 500;
-
-  button {
-    margin: 0 15px;
-  }
-`
+import { Button, ButtonIcon } from 'rmwc/Button';
+import { Select } from 'rmwc/Select';
 
 const PaginationLinks = (props) => {
-  const total = props.total;
-  const current = props.current;
+  const total = parseInt(props.total);
+  const current = parseInt(props.current);
+  let i = 0;
+  const options = Array(total).fill('').map(n => ({
+    label: ++i, value: i
+  }));
+  const prevDisabled = current === 1 ? { disabled: true } : null;
+  const nextDisabled = current === total ? { disabled: true } : null;
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -20,26 +19,34 @@ const PaginationLinks = (props) => {
     props.handlePagination(i);
   }
 
-  if (total === 1) {
+  const handleSelect = (e) => {
+    props.handlePagination(e.target.value);
+  }
+
+  if (total <= 1) {
     return (
       <div></div>
     );
   }
 
   return (
-    <Box>
-      { current > 1 ? (
-        <button className="no-border" page-index={current - 1} onClick={handleClick}>Prev</button>
-      ) : (
-        ''
-      )}
-      { current > 0 ? (<span>{' '}{current} / {total}{' '}</span>) : '' }
-      { current < total ? (
-        <button className="no-border" page-index={current + 1} onClick={handleClick}>Next</button>
-      ) : (
-        ''
-      )}
-    </Box>
+    <div>
+      <Button dense
+        {...prevDisabled}
+        page-index={current - 1}
+        onClick={handleClick}>
+        Prev
+      </Button>
+
+      <Select value={current} options={options} onChange={handleSelect}/>
+
+      <Button dense
+        {...nextDisabled}
+        page-index={current + 1}
+        onClick={handleClick}>
+        Next
+      </Button>
+    </div>
   );
 };
 

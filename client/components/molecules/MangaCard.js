5bcafd88b77fb911f15afd260611afe1ff85d6a7
Nathan Quach <nathanquach8x@gmail.com>
2018-08-26 13:27:24 +1000

    Update MangaCard to use NavLink instead of onCLick for routing

--- client/components/molecules/MangaCard.js
+++ client/components/molecules/MangaCard.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import styled from 'react-emotion';
+import { NavLink } from 'react-router-dom';
 import {
   Card,
   CardPrimaryAction,
@@ -13,75 +14,87 @@ import {
 import { Typography } from 'rmwc/Typography';
 import { Icon } from 'rmwc/Icon';
 
-const StyledCard = styled(Card)`
-  margin: 5px auto;
+const StyledCard = styled(Card)(props => ({
+  margin: '5px auto',
 
-  .mdc-card__media {
-    padding-top: 130%;
-    background-size: contain;
-  }
+  '& .mdc-card__media': {
+    paddingTop: '130%',
+    backgroundSize: 'contain'
+  },
 
-  .mdc-card__action-buttons+.mdc-card__action-icons {
-    margin-left: 0;
-  }
+  '& .mdc-card__action-buttons+.mdc-card__action-icons': {
+    marginLeft: 0
+  },
 
-  .manga-title {
-    margin-bottom: 0;
-    font-weight: 600;
-  }
+  '& .manga-title': {
+    'marginBottom': 0,
+    'fontWeight': 600
+  },
 
-  .tag-groups {
-    position: absolute;
-    padding: 5px;
-    width: 100%;
+  '& .tag-groups': {
+    position: 'absolute',
+    padding: '5px',
+    width: '100%'
+  },
+
+  a: {
+    color: props.theme.onSecondary,
+    textTransform: 'capitalise'
   }
-`;
+}));
 
 export class MangaCard extends PureComponent {
   constructor(props) {
     super(props);
-    this.viewManga = this.viewManga.bind(this);
-  }
-
-  viewManga(e) {
-    this.props.onItemClick(this.props.manga);
   }
 
   render() {
-    const { title, coverPicture, isNew, authorId } = this.props.manga;
+    const { id, title, coverPicture, isNew, authorId } = this.props.manga;
     const author = this.props.authors[authorId] || {};
     const coverUrl = window.location.origin + encodeURI(coverPicture);
+    const mangaUrl = `/mangas/${id}`;
+
     return (
-      <StyledCard>
-        <CardPrimaryAction onClick={this.viewManga}>
-          <CardMedia
-            style={{
-              backgroundImage: `url("${coverUrl}")`
-            }}
-            title={title}
-          />
-          {isNew ?
-            <div className="tag-groups">
-              <Icon use="fiber_new" />
+      <StyledCard theme={this.props.theme}>
+
+        <NavLink to={mangaUrl}>
+          <CardPrimaryAction>
+              <CardMedia
+                style={{
+                  backgroundImage: `url("${coverUrl}")`
+                }}
+                title={title}
+              />
+
+            {isNew ?
+              <div className="tag-groups">
+                <Icon use="fiber_new" />
+              </div>
+            : ''}
+
+            <div style={{ padding: '0 1rem 0' }}>
+              <Typography className="manga-title ellipsis" use="subtitle1" tag="h3">
+                {title}
+              </Typography>
+              <Typography className="ellipsis" use="subtitle2">
+                {author.name}
+              </Typography>
             </div>
-          : ''}
-          <div style={{ padding: '0 1rem 0' }}>
-            <Typography className="manga-title ellipsis" use="subtitle1" tag="h3">
-              {title}
-            </Typography>
-            <Typography className="ellipsis" use="subtitle2">
-              {author.name}
-            </Typography>
-          </div>
-        </CardPrimaryAction>
+          </CardPrimaryAction>
+        </NavLink>
+
         <CardActions>
           <CardActionButtons>
-            <CardAction onClick={this.viewManga}>Read</CardAction>
+            <NavLink to={mangaUrl}>
+              <CardAction>Read</CardAction>
+            </NavLink>
           </CardActionButtons>
+
           <CardActionIcons>
             <CardAction use="more_vert" />
           </CardActionIcons>
         </CardActions>
+
       </StyledCard>
     );
   }
@@ -89,7 +102,8 @@ export class MangaCard extends PureComponent {
 
 const mapStateToProps = (state) => {
   return {
-    authors: state.entities.authors || {}
+    authors: state.entities.authors || {},
+    theme: state.themes[state.enabledTheme]
   };
 };
 

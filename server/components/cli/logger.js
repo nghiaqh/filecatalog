b6ee50f64f2a2b4abc7414fb70b176f7305bd353
Nathan Quach <nathanquach8x@gmail.com>
2018-09-29 21:47:45 +1000

    Change log level

--- server/components/cli/logger.js
+++ server/components/cli/logger.js
@@ -30,8 +30,7 @@ const logger = winston.createLogger({
   level: 'debug',
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({filename: 'error.log', level: 'error'}),
-    new winston.transports.File({filename: 'debug.log', level: 'debug'}),
+    new winston.transports.File({filename: 'error.log', level: 'debug'}),
     new winston.transports.Console({
       format: winston.format.printf(formatMessage)
     })
